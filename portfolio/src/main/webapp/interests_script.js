16842813b55f313533f687d738c933f9833a4b2a
Eshika Saxena <eshikasaxena@outlook.com>
2020-05-29 19:50:12 -0700

    Add JS for slideshow.

---
+++ portfolio/src/main/webapp/interests_script.js
@@ -0,0 +1,42 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var currSlideNum = 0;
+
+function initSlides() {
+    showSlides(currSlideNum);
+}
+
+function advanceSlides(n) {
+    showSlides(currSlideNum += n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("slide");
+    var captionText = document.getElementById("caption");
+    var captions = ["Key Lime Pie", "Mango Cheesecake", "Pear Galette", "Strawb Shortcake"];
+    if (n > slides.length - 1) {
+        currSlideNum = 0;
+    }
+    if (n < 0) {
+        currSlideNum = slides.length - 1;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    console.log(slides);
+    console.log(currSlideNum);
+    slides[currSlideNum].style.display = "block";
+    captionText.innerHTML = captions[n];
+}
